b03794e10140b58941448444f27ddd3a034704bb
Andrii Hutnyk <andreigutnik@gmail.com>
2023-11-02 12:16:08 +0100

    Update contacts.js

--- contacts.js
+++ contacts.js
@@ -22,7 +22,7 @@ async function removeContact(contactId) {
   if (index === -1) {
     return null;
   }
-  const deletedContact = contacts.splice(index, 1);
+  const [deletedContact] = contacts.splice(index, 1);
   await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
   return deletedContact;
 }
